2675f6fcc92a75ca636b0d3dc6b672f5402152f4
Makker5000 <brudetilleux@gmail.com>
2024-10-17 00:09:21 +0200

    Folder of environments variables Removed

--- src/environments/environment.ts
+++
@@ -1,5 +0,0 @@
-export const environment = {
-    production: false,
-    supabaseUrl: "https://lvedrkpmcegwbrphwgly.supabase.co",
-    supabaseKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imx2ZWRya3BtY2Vnd2JycGh3Z2x5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3Mjg0ODMwNzcsImV4cCI6MjA0NDA1OTA3N30.WIo3zv98bZyCvN-E7GZfToSIcoUeBoIC2OgydrSMp6k"
-};

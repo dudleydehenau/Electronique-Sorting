057e5e18386c5ca9c9543f8d51efccb171f16892
Bistou <timothytruong20@hotmail.com>
2024-10-17 00:16:46 +0200

    ajout de la l'inventaire des composants sur la page d'accueil

--- src/app/composants/composants.component.ts
+++ src/app/composants/composants.component.ts
@@ -1,12 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { MatCardModule } from '@angular/material/card';
+import { MatChipsModule } from '@angular/material/chips';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { SupabaseService } from './supabase.service';
+
+// export interface ComponentCard {
+//   cols: number;
+//   rows: number;
+//   carac: string;
+//   quantity: number;
+//   name: string;
+// }
 
 @Component({
   selector: 'app-composants',
   standalone: true,
-  imports: [],
+  imports: [MatCardModule, MatChipsModule, MatGridListModule],
   templateUrl: './composants.component.html',
-  styleUrl: './composants.component.scss'
+  styleUrl: './composants.component.scss',
 })
-export class ComposantsComponent {
+export class ComposantsComponent implements OnInit {
+  tiles: any[] = [];
 
+  constructor(private supabaseService: SupabaseService) {}
+
+  ngOnInit(): void {
+    this.getInventory();
+  }
+
+  getInventory(): void {
+    this.supabaseService.getInventory().then((data) => {
+      this.tiles = data;
+    });
+  }
 }
